b5a23108717d5942a673bffccf35edbddbfb2392
Cloyd Abad <abadcloyd@gmail.com>
2023-09-18 20:12:01 +0800

    finished

--- main.go
+++ main.go
@@ -2,27 +2,65 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"math"
 	"os"
 )
 
 type dataStruct struct {
-	min  int
-	max  int
-	data []int
+	Min  int   `json:"min"`
+	Max  int   `json:"max"`
+	Data []int `json:"data"`
 }
 
 func main() {
-	// equalWidth(3)
-	data := equalFrequency(3)
-	log.Println(data)
-	// smoothingMeans(data)
-	// log.Println(data)
-	// smoothingMedian(data)
-	// log.Println(data)
-	smoothingBounderies(data)
-	log.Println(data)
+	var bins, choice int
+	fmt.Println("BINNING DATA")
+	fmt.Println("Make sure your data.json file is within the same directory")
+	fmt.Println("Please enter your choice:")
+	fmt.Println("1. Equal Width Binning")
+	fmt.Println("2. Equal Frequency Binning")
+	fmt.Println("3. Equal Frequency Binning (Smoothing by Means)")
+	fmt.Println("4. Equal Frequency Binning (Smoothing by Bounderies)")
+	fmt.Println("5. Equal Frequency Binning (Smoothing by Median)")
+	fmt.Println("Enter choice: ")
+	fmt.Scan(&choice)
+	fmt.Println("Enter bins: ")
+	fmt.Scan(&bins)
+
+	var finalData []dataStruct
+	if choice == 1 {
+		finalData = equalWidth(bins)
+	}
+	if choice == 2 {
+		finalData = equalFrequency(bins)
+	}
+	if choice == 3 {
+		finalData = equalFrequency(bins)
+		smoothingMeans(finalData)
+	}
+	if choice == 4 {
+		finalData = equalFrequency(bins)
+		smoothingBounderies(finalData)
+	}
+	if choice == 5 {
+		finalData = equalFrequency(bins)
+		smoothingMedian(finalData)
+	}
+
+	// Writing the file
+	file, err := os.Create("final_data.json")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close()
+	encoder := json.NewEncoder(file)
+	if err := encoder.Encode(finalData); err != nil {
+		fmt.Println("Error encoding data to JSON:", err)
+		return
+	}
 }
 
 func equalWidth(bins int) []dataStruct {
@@ -36,22 +74,22 @@ func equalWidth(bins int) []dataStruct {
 
 	wCount := 0
 	for i := 0; i < bins; i++ {
-		finalData[i].min = wCount + 1
+		finalData[i].Min = wCount + 1
 		if i == 0 {
-			finalData[i].min = wCount
+			finalData[i].Min = wCount
 		}
-		finalData[i].max = wCount + w
+		finalData[i].Max = wCount + w
 		wCount = wCount + w
 	}
 	for i, itemFinal := range finalData {
 		for _, item := range data {
-			if itemFinal.min > item {
+			if itemFinal.Min > item {
 				continue
 			}
-			if itemFinal.max >= item {
-				finalData[i].data = append(finalData[i].data, item)
+			if itemFinal.Max >= item {
+				finalData[i].Data = append(finalData[i].Data, item)
 			}
-			if itemFinal.max < item {
+			if itemFinal.Max < item {
 				break
 			}
 		}
@@ -67,19 +105,19 @@ func equalFrequency(bins int) []dataStruct {
 	finalData := make([]dataStruct, bins)
 
 	for _, item := range data {
-		if len(finalData[i].data) == length && i+1 < bins {
-			finalData[i].max = finalData[i].data[len(finalData[i].data)-1]
+		if len(finalData[i].Data) == length && i+1 < bins {
+			finalData[i].Max = finalData[i].Data[len(finalData[i].Data)-1]
 			i += 1
-			finalData[i].min = finalData[i-1].data[len(finalData[i-1].data)-1] + 1
+			finalData[i].Min = finalData[i-1].Data[len(finalData[i-1].Data)-1] + 1
 		}
-		if len(finalData[i].data) >= length && i+2 > bins {
-			finalData[i].data = append(finalData[i].data, item)
-			finalData[i].max = finalData[i].data[len(finalData[i].data)-1]
+		if len(finalData[i].Data) >= length && i+2 > bins {
+			finalData[i].Data = append(finalData[i].Data, item)
+			finalData[i].Max = finalData[i].Data[len(finalData[i].Data)-1]
 			continue
 		}
-		if len(finalData[i].data) <= length {
-			finalData[i].data = append(finalData[i].data, item)
-			finalData[i].max = finalData[i].data[len(finalData[i].data)-1]
+		if len(finalData[i].Data) <= length {
+			finalData[i].Data = append(finalData[i].Data, item)
+			finalData[i].Max = finalData[i].Data[len(finalData[i].Data)-1]
 			continue
 		}
 	}
@@ -90,32 +128,32 @@ func smoothingMeans(data []dataStruct) {
 	for i, slice := range data {
 		total := 0
 		means := 0
-		for _, item := range slice.data {
+		for _, item := range slice.Data {
 			total += item
 		}
-		means = total / len(slice.data)
-		for k := range slice.data {
-			data[i].data[k] = means
+		means = total / len(slice.Data)
+		for k := range slice.Data {
+			data[i].Data[k] = means
 		}
 	}
 }
 func smoothingMedian(data []dataStruct) {
 	for i, slice := range data {
-		for k := range slice.data {
-			index := int(math.Floor(float64(len(slice.data) / 2)))
-			data[i].data[k] = slice.data[int(index)]
+		for k := range slice.Data {
+			index := int(math.Floor(float64(len(slice.Data) / 2)))
+			data[i].Data[k] = slice.Data[int(index)]
 		}
 	}
 }
 func smoothingBounderies(data []dataStruct) {
 	for i, slice := range data {
-		for k := range slice.data {
-			index := int(math.Floor(float64(len(slice.data) / 2)))
+		for k := range slice.Data {
+			index := int(math.Floor(float64(len(slice.Data) / 2)))
 			if k <= index {
-				data[i].data[k] = slice.data[0]
+				data[i].Data[k] = slice.Data[0]
 			}
 			if k > index {
-				data[i].data[k] = slice.data[len(slice.data)-1]
+				data[i].Data[k] = slice.Data[len(slice.Data)-1]
 			}
 		}
 	}
